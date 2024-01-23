0feaca8b68d2dfb0a40c14e6f3be25e31448e110
ABDULMALIK MAHMUD <122923436+MalikPRO1@users.noreply.github.com>
2024-01-23 23:34:27 +0000

    Delete backend/middleware directory

--- backend/middleware/logger.middleware.js
+++
@@ -1,7 +0,0 @@
-// logger.middleware.js - Middleware that outputs all requests to the server console
-const loggerMiddleware = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-};
-
-module.exports = loggerMiddleware;
--- backend/middleware/staticFile.middleware.js
+++
@@ -1,8 +0,0 @@
-// staticFile.middleware.js - Middleware to serve static files (lesson images)
-const express = require('express');
-const path = require('path');
-
-const staticPath = path.join(__dirname, 'lesson-images');
-const staticFileMiddleware = express.static(staticPath);
-
-module.exports = staticFileMiddleware;
