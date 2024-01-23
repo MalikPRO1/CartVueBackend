51817c6d898cd47c16c61124dd9d131e896b0049
ABDULMALIK MAHMUD <122923436+MalikPRO1@users.noreply.github.com>
2024-01-23 23:39:40 +0000

    Delete backend/server.js

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-// server.js - Entry point for your Express.js server
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
